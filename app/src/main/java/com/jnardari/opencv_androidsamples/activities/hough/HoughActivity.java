979c90ed7215e40061ea4e1373470d842765131e
Daeki Kwon <dkkwon0228@gmail.com>
2017-09-14 13:04:20 +0900

    remove unused  import statement.

--- app/src/main/java/com/jnardari/opencv_androidsamples/activities/hough/HoughActivity.java
+++ app/src/main/java/com/jnardari/opencv_androidsamples/activities/hough/HoughActivity.java
@@ -1,6 +1,5 @@
 package com.jnardari.opencv_androidsamples.activities.hough;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
